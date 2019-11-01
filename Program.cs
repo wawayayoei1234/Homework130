d5eefeb70dd39dadd2bd76cb48baf75220006b64
vissavut <51730920+vissavut@users.noreply.github.com>
2019-11-01 13:15:06 +0700

    vissavut/13/2
    vissavut/13/2

--- Program.cs
+++ Program.cs
@@ -1,102 +1,105 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-    public interface IHomework05
+namespace test
 {
-    string DisplayLEDOnScreen(string ledNo);
-}
-
-    public class Homework05 : IHomework05
-    {
-       public string DisplayLEDOnScreen(string ledNo)
-        {
-           
-              int x = 0;
-            string[] d = { "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]", "[ ]" };
-            int[] num = {0,0,0,0,0,0,0,0,0,0};
-            string e = (" 1  2  3  4  5  6  7  8  9  A");
-           
-             Console.WriteLine  ("Please choose LED to turn On/Off");
-            Console.WriteLine(d[0]+d[1]+d[2]+d[3]+d[4]+d[5]+d[6]+d[7]+d[8]+d[9]);
-            Console.WriteLine(e);
-            Console.WriteLine("LED OFF");
-
-
-            while(x<10){
-             Console.WriteLine  ("Please choose LED to turn On/Off");
-           int input = int.Parse(Console.ReadLine());
-            var ww = "[!]";
-            var dd = "[ ]";
-            int b = 1;
-            b = input - b;
-            int Ledpi = 0; 
-             Ledpi = b + 1;
-
-
-            
-            
-            if (input <=10)
-            {
-
-             if(1 == num[b] )
-                {
-                
-                    d[b] = dd;
-                    Console.WriteLine(d[0]+d[1]+d[2]+d[3]+d[4]+d[5]+d[6]+d[7]+d[8]+d[9]);
-                    Console.WriteLine(e);
-                   
-                    num[b] = 0;    
-                     Console.WriteLine("LED "+Ledpi+" OFF");                                           
-                  
-                 }
-              else     
-                 {           
-                 
-                 
-                    d[b] = ww;
-                    Console.WriteLine(d[0]+d[1]+d[2]+d[3]+d[4]+d[5]+d[6]+d[7]+d[8]+d[9]);
-                    Console.WriteLine(e);
-                     
-                    num[b] = 1;   
-                    Console.WriteLine("LED "+Ledpi+" ON");           
-                  }
-            
-            }   
-            else  
-            {
-                Console.WriteLine("LED Error ");
-            }
-            
-      
-        }
-           return ledNo;
-        }
-       
-
-            
-               
-
-           
-        
-    }
-    class Program 
+    class Program
     {
         static void Main(string[] args)
+
         {
+            IEnumerable<int> oop = null;
+            string nums = null; // string.Empty;
+            string myString = string.Empty;
+            int myInt;
+            int max = 4;
+
+            Console.WriteLine("ป้อนค่่าตัวเลข 6 หลัก จำนวน 4 ค่า");
+            for (int i = 0; i < max; i++)
+            {   
+                try
+                {
+                    myString = Console.ReadLine();  //รับค่า
+
+                    var array = myString.ToCharArray() .Where(x => int.TryParse
+                            (x.ToString(), out myInt))
+                            .Select(x => int.Parse
+                            (x.ToString())).ToArray();  //แปลง string to array
+
+                   var input = new run();        
+                   oop = input.Homework13(array);
+               
+                   foreach (var item in oop)
+                     {
+                        nums+=item;
+                     }
+                    
+                }
+                catch (System.Exception)
+                {
+                    
+                    Console.WriteLine("Error");
+                }
+                  
+             }
+             
+         Console.WriteLine("**99**");
+         Console.WriteLine(nums);             
+                  
+        }    
+    }    
+                 
+}             
+                  
+            
+           
+                                    
+      
+  
+            
+           
+
+           
+               
+                
+                       
+                   
+                  
+  
+
          
-          Homework05 test = new Homework05();
-          test.DisplayLEDOnScreen("");
-
-         }
-
-        
-}        
-       
+             
             
-        
             
-        
+         
           
-        
+                
+                     
+                 
 
-    
+            
+                 
+                      
+            
+            
+                   
+
+                  
+                       
+                     
+               
+                       
+            
+        
+                 
+                   
+           
+
+
+
+
+
+
+
 
